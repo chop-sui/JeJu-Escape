d9ef88b9305770dc4844e3025a63a92b99a959b0
JeongJae Park <jeongjae1996@naver.com>
2019-11-24 20:12:27 +0900

    퀴즈 정답 오류 수정

--- 최종/merged.js
+++ 최종/merged.js
@@ -1,101 +1,101 @@
 Function.prototype.member = function(name, value){
-	this.prototype[name] = value
+   this.prototype[name] = value
 }
 
 //////// Game Definition
 function Game(){}
 Game.start = function(room, welcome){
-	game.start(room.id)
-	printMessage(welcome)
+   game.start(room.id)
+   printMessage(welcome)
 }
 Game.end = function(){
-	game.clear()
+   game.clear()
 }
 Game.move = function(room){
-	game.move(room.id)
+   game.move(room.id)
 }
 Game.handItem = function(){
-	return game.getHandItem()
+   return game.getHandItem()
 }
 
 //////// Room Definition
 function Room(name, background){
-	this.name = name
-	this.background = background
-	this.id = game.createRoom(name, background)
+   this.name = name
+   this.background = background
+   this.id = game.createRoom(name, background)
 }
 Room.member('setRoomLight', function(intensity){
-	this.id.setRoomLight(intensity)
+   this.id.setRoomLight(intensity)
 })
 
 //////// Object Definition
 function Object(room, name, image){
-	this.room = room
-	this.name = name
-	this.image = image
+   this.room = room
+   this.name = name
+   this.image = image
 
-	if (room !== undefined){
-		this.id = room.id.createObject(name, image)
-	}
+   if (room !== undefined){
+      this.id = room.id.createObject(name, image)
+   }
 }
 Object.STATUS = { OPENED: 0, CLOSED: 1, LOCKED: 2 }
 
 Object.member('setSprite', function(image){
-	this.image = image
-	this.id.setSprite(image)
+   this.image = image
+   this.id.setSprite(image)
 })
 Object.member('resize', function(width){
-	this.id.setWidth(width)
+   this.id.setWidth(width)
 })
 Object.member('setDescription', function(description){
-	this.id.setItemDescription(description)
+   this.id.setItemDescription(description)
 })
 
 Object.member('getX', function(){
-	return this.id.getX()
+   return this.id.getX()
 })
 Object.member('getY', function(){
-	return this.id.getY()
+   return this.id.getY()
 })
 Object.member('locate', function(x, y){
-	this.room.id.locateObject(this.id, x, y)
+   this.room.id.locateObject(this.id, x, y)
 })
 Object.member('move', function(x, y){
-	this.id.moveX(x)
-	this.id.moveY(y)
+   this.id.moveX(x)
+   this.id.moveY(y)
 })
 Object.member('show', function(){
-	this.id.show()
+   this.id.show()
 })
 Object.member('hide', function(){
-	this.id.hide()
+   this.id.hide()
 })
 Object.member('open', function(){
-	this.id.open()
+   this.id.open()
 })
 Object.member('close', function(){
-	this.id.close()
+   this.id.close()
 })
 Object.member('lock', function(){
-	this.id.lock()
+   this.id.lock()
 })
 Object.member('unlock', function(){
-	this.id.unlock()
+   this.id.unlock()
 })
 Object.member('isOpened', function(){
-	return this.id.isOpened()
+   return this.id.isOpened()
 })
 Object.member('isClosed', function(){
-	return this.id.isClosed()
+   return this.id.isClosed()
 })
 Object.member('isLocked', function(){
-	return this.id.isLocked()
+   return this.id.isLocked()
 })
 Object.member('pick', function(){
-	this.id.pick()
+   this.id.pick()
 })
 Object.member('isPicked', function(){
-	return this.id.isPicked()
+   return this.id.isPicked()
 })
 
 ////// MoveRoom Definition
@@ -114,38 +114,38 @@ MoveRoom.member('onClick', function(){
 
 ///// MoveRoom_Print Definition
 function MoveRoom_Print(room, name, image, connectedTo, message) {
-	MoveRoom.call(this, room, name, image, connectedTo)
+   MoveRoom.call(this, room, name, image, connectedTo)
 
-	this.message = message
+   this.message = message
 
 }
 
 MoveRoom_Print.prototype = new MoveRoom()
 
 MoveRoom_Print.member('onClick', function() {
-	Game.move(this.connectedTo)
-	printMessage(this.message)
+   Game.move(this.connectedTo)
+   printMessage(this.message)
 })
 
 /////// Item Definition
 function Item(room, name, image){
-	Object.call(this, room, name, image)
+   Object.call(this, room, name, image)
 }
 // inherited from Object
 Item.prototype = new Object()
 
 Item.member('onClick', function(){
-	this.id.pick()
+   this.id.pick()
 })
 Item.member('isHanded', function(){
-	return Game.handItem() == this.id
+   return Game.handItem() == this.id
 })
 
 ///// Conversation Definition
 function Conversation(room, name, image) {
-	Object.call(this, room, name, image)
+   Object.call(this, room, name, image)
 
-	this.id.hide()
+   this.id.hide()
 }
 
 Conversation.prototype = new Object()
@@ -156,50 +156,50 @@ Conversation.member('onClick', function() {
 
 //////// Keypad Definition
 function Keypad(room, name, image, password, callback, type){
-	Object.call(this, room, name, image)
+   Object.call(this, room, name, image)
 
-	// Keypad properties
-	this.password = password
-	this.callback = callback
+   // Keypad properties
+   this.password = password
+   this.callback = callback
   this.type = type
 }
 // inherited from Object
 Keypad.prototype = new Object()
 
 Keypad.member('onClick', function(){
-	showKeypad(this.type, this.password, this.callback)
+   showKeypad(this.type, this.password, this.callback)
 })
 
 //////// Door Definition
 function Door(room, name, closedImage, openedImage, connectedTo){
-	Object.call(this, room, name, closedImage)
+   Object.call(this, room, name, closedImage)
 
-	// Door properties
-	this.closedImage = closedImage
-	this.openedImage = openedImage
-	this.connectedTo = connectedTo
+   // Door properties
+   this.closedImage = closedImage
+   this.openedImage = openedImage
+   this.connectedTo = connectedTo
 }
 // inherited from Object
 Door.prototype = new Object()
 
 Door.member('onClick', function(){
-	if (!this.id.isLocked() && this.id.isClosed()){
-		this.id.open()
-	}
-	else if (this.id.isOpened()){
-		if (this.connectedTo !== undefined){
-			Game.move(this.connectedTo)
-		}
-		else {
-			Game.end()
-		}
-	}
+   if (!this.id.isLocked() && this.id.isClosed()){
+      this.id.open()
+   }
+   else if (this.id.isOpened()){
+      if (this.connectedTo !== undefined){
+         Game.move(this.connectedTo)
+      }
+      else {
+         Game.end()
+      }
+   }
 })
 Door.member('onOpen', function(){
-	this.id.setSprite(this.openedImage)
+   this.id.setSprite(this.openedImage)
 })
 Door.member('onClose', function(){
-	this.id.setSprite(this.closedImage)
+   this.id.setSprite(this.closedImage)
 })
 
 ///// 방 생성
@@ -323,8 +323,8 @@ fish_diner.conv1.resize(1280)
 fish_diner.conv1.locate(640, 600)
 
 // 퀴즈1 정답 키패드 생성
-fish_diner.answer1 = new Keypad(fish_diner, 'answer1', '퀴즈1.png', '904', function(){
-  printMessage('맞아맞아 904호였지 금방 다녀올게 아이패드로 퀴즈라도 풀고 있어~')
+fish_diner.answer1 = new Keypad(fish_diner, 'answer1', '퀴즈1.png', '709', function(){
+  printMessage('맞아맞아 709호였지 금방 다녀올게 아이패드로 퀴즈라도 풀고 있어~')
   fish_diner.owner.hide()
   fish_diner.answer1.hide()
   fish_diner.ipad.show()
@@ -340,7 +340,7 @@ fish_diner.ipad.hide()
 // 대화
 // 주인 누르면 대화 상자 show.
 fish_diner.owner.onClick = function() {
-	fish_diner.conv1.show()
+   fish_diner.conv1.show()
 }
 
 fish_diner.conv1.onClick = function() {
