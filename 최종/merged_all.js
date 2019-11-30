55f263d5a90181a5f98b032430f0196149ab6887
suu lee <2swimming96@naver.com>
2019-11-30 11:22:56 +0900

    room1 완성합침, 시장버튼hideshow

--- 최종/merged_all.js
+++ 최종/merged_all.js
@@ -106,12 +106,11 @@ Object.member('isPicked', function(){
 function MoveRoom(room, name, image, connectedTo) {
    Object.call(this, room, name, image)
 
-   // this.image = image
+   //this.image = image
    this.connectedTo = connectedTo
 }
 
 MoveRoom.prototype = new Object()
-
 MoveRoom.member('onClick', function(){
    Game.move(this.connectedTo)
 })
@@ -229,19 +228,19 @@ Drawer.member('onClose', function(){
 })
 
 //////// Keypad Definition
-function Keypad(room, name, image, password, callback, type){
+function Keypad(room, name, image, password, callback){
     Object.call(this, room, name, image)
 
     // Keypad properties
     this.password = password
     this.callback = callback
-    this.type = type
+
  }
  // inherited from Object
  Keypad.prototype = new Object()
 
  Keypad.member('onClick', function(){
-    showKeypad(this.type, this.password, this.callback)
+	showKeypad('number', this.password, this.callback)
 })
 
 //////// DoorLock Definition
@@ -285,7 +284,6 @@ Door.member('onOpen', function(){
 Door.member('onClose', function(){
     this.id.setSprite(this.closedImage)
 })
-<<<<<<< HEAD
  
 
 //////// DoorLock Definition
@@ -298,16 +296,17 @@ function DoorLock(room, name, image, password, door, type, message){
  // inherited from Object
  DoorLock.prototype = new Keypad()
  
+
+
+ ///// Wallet Definition
+  function Wallet (room,name,image){
+      Item.call(this,room,name,image)
+  }
+
+  // inherited from Item
+  Wallet.prototype=new Item()
  
  
- 
-=======
-
-
-
-
-
->>>>>>> master
 
 // =======================================================================================================================
 ///////// Make Room
@@ -351,7 +350,7 @@ airport = new Room('airport', '공항.png')
 ///////// House
 
 
-///// 2층 복도
+///// 2층 복도 ///////
 // 우도방이동 (주인공방)
 hallway.item1 = new MoveRoom_Print(hallway, 'item1', '우도.png', room1_mainview, '내 방인 우도방으로 들어왔다.')
 hallway.item1.resize(200)
@@ -376,11 +375,16 @@ hallway.item4.locate(150, 300)
 
 
 
-///////주인공방
+
+////////// 주인공 방 //////////
+/////메인 뷰
+
+//침대
 room1_mainview.bed = new Object(room1_mainview, 'bed', 'bed.png')
 room1_mainview.bed.resize(700)
 room1_mainview.bed.locate(740, 470)
 
+//가방
 room1_mainview.bag = new Object(room1_mainview, 'bag', 'bag_closed.png')
 room1_mainview.bag.resize(200)
 room1_mainview.bag.locate(270, 650)
@@ -392,6 +396,7 @@ room1_mainview.bag.onClick = function()
 	printMessage('어? 내 여권, 내 지갑.. 소지품이 다 없어졌어..!')
 }
 
+//탁자
 room1_mainview.sidetable = new Sidetable(room1_mainview, 'sidetable', 'sidetable_closed.png', 'sidetable_opened.png', room1_sidetableview)
 room1_mainview.sidetable.resize(170)
 room1_mainview.sidetable.locate(380,500)
@@ -405,7 +410,9 @@ room1_mainview.keypad1_closed.onClick = function(){
 	Game.move(room1_sidetableview)
 }
 
+/////탁자 뷰
 
+//탁자
 room1_sidetableview.sidetable = new Sidetable(room1_sidetableview, 'sidetable', 'sidetable2_closed.png', 'sidetable2_opened.png')
 room1_sidetableview.sidetable.resize(400)
 room1_sidetableview.sidetable.locate(300, 300)
@@ -428,24 +435,25 @@ room1_sidetableview.sidetable.onClick = function(){
 	}
 }
 
-room1_sidetableview.keypad2_closed = new DoorLock(room1_sidetableview, 'keypad2_closed', 'keypad2_closed.png', '1111', room1_sidetableview.sidetable, 'number', '철커덕')
+//탁자 키패드
+room1_sidetableview.keypad2_closed = new DoorLock(room1_sidetableview, 'keypad2_closed', 'keypad2_closed.png', '0884', room1_sidetableview.sidetable, 'number', '철커덕')
 room1_sidetableview.keypad2_closed.resize(150)
 room1_sidetableview.keypad2_closed.locate(415, 283)
 
+//휴대폰
 room1_sidetableview.phone = new Item(room1_sidetableview, 'phone', '핸드폰.png')
 room1_sidetableview.phone.resize(100)
 room1_sidetableview.phone.locate(343, 340)
 room1_sidetableview.phone.hide()
+room1_sidetableview.phone.setDescription('휴대폰을 어디에 사용할 수 있을까..?')
 
 room1_sidetableview.phone.onClick = function(){
 	printMessage('엥? 내 휴대폰이 왜 여기에 있지?')
 	this.id.pick()
 }
+room1_sidetableview.phone.setDescription("밧데리가 없어서 안켜진다.. 충전기를 찾아봐야겠어")
 
-
-
-
-
+//이동 화살표
 room1_sidetableview.leftarrow = new MoveRoom(room1_sidetableview, 'leftarrow', 'arrow_left.png', room1_mainview)
 room1_sidetableview.leftarrow.resize(120)
 room1_sidetableview.leftarrow.locate(55, 350)
@@ -454,6 +462,9 @@ room1_mainview.rightarrow = new MoveRoom(room1_mainview, 'rightarrow', 'arrow_ri
 room1_mainview.rightarrow.resize(120)
 room1_mainview.rightarrow.locate(1250, 400)
 
+/////우측 뷰
+
+//이동 화살표
 room1_rightview.rightarrow = new MoveRoom(room1_rightview, 'rightarrow', 'arrow_right.png', hallway)
 room1_rightview.rightarrow.resize(120)
 room1_rightview.rightarrow.locate(1150, 400)
@@ -462,11 +473,18 @@ room1_rightview.leftarrow = new MoveRoom(room1_rightview, 'leftarrow', 'arrow_le
 room1_rightview.leftarrow.resize(120)
 room1_rightview.leftarrow.locate(150, 400)
 
-
+//이동 스팟(티비)
 room1_rightview.tv_click = new MoveRoom(room1_rightview, 'tv_click', 'click_spot.png', room1_tvview)
 room1_rightview.tv_click.resize(320)
 room1_rightview.tv_click.locate(780, 340)
 
+room1_rightview.drawer_click = new MoveRoom(room1_rightview, 'drawer_click', 'click_spot.png', room1_drawerview)
+room1_rightview.drawer_click.resize(320)
+room1_rightview.drawer_click.locate(780, 578)
+
+/////티비 뷰
+
+//이동 화살표
 room1_tvview.downarrow = new MoveRoom(room1_tvview, 'downarrow', 'arrow_down.png', room1_rightview)
 room1_tvview.downarrow.resize(80)
 room1_tvview.downarrow.locate(610, 660)
@@ -476,53 +494,108 @@ room1_tvview.downarrow.onClick = function(){
 	room1_tvview.tv_off.setSprite('tv_off.png')
 }
 
-room1_rightview.drawer_click = new MoveRoom(room1_rightview, 'drawer_click', 'click_spot.png', room1_drawerview)
-room1_rightview.drawer_click.resize(320)
-room1_rightview.drawer_click.locate(780, 578)
+//티비
+room1_tvview.tv_off = new Object(room1_tvview, 'tv_off', 'tv_off.png')
+room1_tvview.tv_off.move(-5, -47)
+room1_tvview.tv_off.onClick = function(){
+	if (room1_sidetableview.phone.isHanded()){
+		this.id.setSprite('tv_off.png')
+		room1_tvview.icon_note.show()
+		room1_tvview.icon_find.show()
+		room1_tvview.todo.hide()
+		room1_tvview.map.hide()
+	}
+	else{
+		this.id.setSprite('tv_error.png')
+		printMessage('휴대폰을 연결해봐야겠다.')
+	}
+}
 
+//위치추적 아이콘
+room1_tvview.icon_find = new Object(room1_tvview, 'icon_find', 'icon_find.png')
+room1_tvview.icon_find.resize(140)
+room1_tvview.icon_find.locate(500, 300)
+room1_tvview.icon_find.hide()
+
+room1_tvview.icon_find.onClick = function(){
+	room1_tvview.map.show()
+	printMessage('어? 어제갔던 시장에 내 에어팟이??..')
+	this.id.hide()
+	room1_tvview.icon_note.hide()
+}
+
+//메모장 아이콘
+room1_tvview.icon_note = new Object(room1_tvview, 'icon_note', 'icon_note.png')
+room1_tvview.icon_note.resize(140)
+room1_tvview.icon_note.locate(770, 300)
+room1_tvview.icon_note.hide()
+
+room1_tvview.icon_note.onClick = function(){
+	room1_tvview.todo.show()
+	printMessage('챙겨야 할 것들이군..')
+	this.id.hide()
+	room1_tvview.icon_find.hide()
+}
+
+//위치추적 이미지
+room1_tvview.map = new Object(room1_tvview, 'map', '위치추적.png')
+room1_tvview.map.resize(230)
+room1_tvview.map.locate(610, 310)
+room1_tvview.map.hide()
+
+room1_tvview.map.onClick = function(){
+	this.id.hide()
+	room1_tvview.icon_find.show()
+    room1_tvview.icon_note.show()
+    market.move1.show()
+
+}
+
+//메모장 이미지
+room1_tvview.todo = new Object(room1_tvview, 'todo', '폰메모장.png')
+room1_tvview.todo.resize(230)
+room1_tvview.todo.locate(610, 310)
+room1_tvview.todo.hide()
+
+room1_tvview.todo.onClick = function(){
+	this.id.hide()
+	room1_tvview.icon_find.show()
+	room1_tvview.icon_note.show()
+}
+
+/////서랍 뷰
+
+//이동 화살표
 room1_drawerview.downarrow = new MoveRoom(room1_drawerview, 'downarrow', 'arrow_down.png', room1_rightview)
 room1_drawerview.downarrow.resize(80)
 room1_drawerview.downarrow.locate(610, 660)
 
+//서랍
 room1_drawerview.drawer_closed = new Drawer(room1_drawerview, 'drawer_closed', 'drawer_closed.png', 'drawer_opened.png')
 room1_drawerview.drawer_closed.locate(765, 508)
 
 Drawer.member('onClick', function(){
 	if (this.id.isClosed()){
 		this.id.open()
+		room1_drawerview.note.show()
 	}
-
+	
 	else if (this.id.isOpened()){
 		this.id.close()
+		room1_drawerview.note.hide()
 	}
-	room1_drawerview.charger.show()
+	
 })
 
-room1_drawerview.charger = new Item(room1_drawerview, 'charger', 'charger.png')
-room1_drawerview.charger.resize(100)
-room1_drawerview.charger.locate(760, 555)
-room1_drawerview.charger.hide()
+//쪽지
+room1_drawerview.note = new Object(room1_drawerview, 'note', '쪽지.png')
+room1_drawerview.note.resize(100)
+room1_drawerview.note.locate(760, 555)
+room1_drawerview.note.hide()
 
-room1_drawerview.phone_connected = new Object(room1_drawerview, 'phone_connected', 'phone_connected.png')
-room1_drawerview.phone_connected.resize(40)
-room1_drawerview.phone_connected.hide()
-
-//game.makeCombination(room1_sidetableview.phone, room1_drawerview.charger, room1_drawerview.phone_connected)
-
-room1_tvview.tv_off = new Object(room1_tvview, 'tv_off', 'tv_off.png')
-room1_tvview.tv_off.move(-5, -47)
-room1_tvview.tv_off.onClick = function(){
-	this.id.setSprite('tv_error.png')
+room1_drawerview.note.onClick = function(){
+	showImageViewer('쪽지내용.png', '')
 }
-room1_tvview.icon_find = new Object(room1_tvview, 'icon_find', 'icon_find.png')
-room1_tvview.icon_find.resize(140)
-room1_tvview.icon_find.locate(500, 300)
-room1_tvview.icon_find.hide()
-
-room1_tvview.icon_note = new Object(room1_tvview, 'icon_note', 'icon_note.png')
-room1_tvview.icon_note.resize(140)
-room1_tvview.icon_note.locate(770, 300)
-room1_tvview.icon_note.hide()
 
 
 
@@ -583,18 +656,12 @@ f_room.door.locate(370, 370)
 
 
 
-<<<<<<< HEAD
 //////빈방 
 //문
 room2.door_to_hallway = new MoveRoom(room2, 'door_to_hallway', '문-우-열림.png', hallway)
 room2.door_to_hallway.resize(120)
 room2.door_to_hallway.locate(1100,310)
 
-=======
-
-
-//////빈방
->>>>>>> master
 room2.bad = new Object(room2, 'bad', '침대-2.png')
 room2.bad.resize(400)
 room2.bad.locate(550,400)
@@ -632,7 +699,7 @@ room2.G3.locate(250, 310)
 
 
 
-////// 1층 거실
+///////////////// 1층 거실 ////////////////////////////
 // 소주,생선
 living_room.item1 = new Object(living_room, 'item1', '소주+생선.png')
 living_room.item1.resize(300)
@@ -640,6 +707,7 @@ living_room.item1.locate(730, 540)
 living_room.item1.onClick = function()
 {
     printMessage('어제 갈치를 먹었나...')
+    market.move2.show()
 }
 
 // 쓰레기
@@ -669,7 +737,7 @@ living_room.laundry.locate(1130, 650)
 
 
 
-///// 세탁실
+///////// 세탁실 ///////////////
 //문
 laundry.door = new Door(laundry, 'door', '방문_닫.png', '방문_열.png', living_room)
 laundry.door.resize(230)
@@ -692,10 +760,14 @@ laundry.washer.onClick = function(){
 }
 
 //지갑
-laundry.wallet = new Item(laundry, 'wallet', '지갑_열.png')
+laundry.wallet = new Wallet(laundry, 'wallet', '지갑_열.png')
 laundry.wallet.resize(100)
 laundry.wallet.locate(800, 600)
 laundry.wallet.hide()
+Wallet.member('onClick',function(){
+    this.id.pick()
+    market.move3.show()
+})
 laundry.wallet.setDescription('기념품을 산 영수증이 들어있네..!')
 
 
@@ -773,21 +845,28 @@ olle_ent2.car.onClick = function(){
 
 
 
-///// 시장내부
+///////////////// 시장내부 /////////////////
 // 콩나물 가게 이동.
 market.move1 = new MoveRoom_Print(market, 'move1', '콩나물 가게 이동.png', bean_shop, 'GPS 상으론 여기에 에어팟이 있다고 나오는데...?')
 market.move1.resize(150)
 market.move1.locate(550, 500)
+market.move1.hide()
+
 
 // 갈치 식당 이동.
 market.move2 = new MoveRoom(market, 'move2', '갈치 식당 이동.png', fish_diner)
 market.move2.resize(150)
 market.move2.locate(730, 500)
+market.move2.hide()
 
 // 기념품 가게 이동.
 market.move3 = new MoveRoom(market, 'move3', '기념품 가게 이동.png', gift_shop)
 market.move3.resize(150)
 market.move3.locate(550, 600)
+market.move3.hide()
+
+
+
 
 // 시장 입구로 이동.
 market.move4 = new MoveRoom(market, 'move4', '시장 입구 이동.png', olle_ent2)
@@ -805,7 +884,7 @@ bean_shop.airpods.locate(500, 400)
 
 // 시장으로 이동.
 bean_shop.toMarket = new MoveRoom(bean_shop, 'toMarket', '화살표.png', market)
-bean_shop.toMarket.resize(70)
+bean_shop.toMarket.resize(100)
 bean_shop.toMarket.locate(217, 600)
 
 
@@ -886,7 +965,7 @@ fish_diner.toMarket.locate(50, 350)
 
 
 
-///// 기념품 가게
+//////////// 기념품 가게 //////////////
 
  
 
@@ -953,6 +1032,10 @@ airport.crew.onClick = function(){
     }
 }
 
+// 시장으로 이동.
+airport.toMarket = new MoveRoom(airport, 'toMarket', '화살표.png', market)
+airport.toMarket.resize(100)
+airport.toMarket.locate(150, 600)
 
 
 
